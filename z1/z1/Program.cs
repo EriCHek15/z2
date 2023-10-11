105a788b227c5b4033d59c4d02a496ecc98e0b19
EriCHek15 <eriksokol2@gmail.com>
2023-10-11 14:03:23 +0300

    z2 v1

---
+++ z1/z1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace z1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод строки
+            Console.WriteLine("Введите исходную строку");
+            string a = Console.ReadLine();
+
+            Regex str = new Regex(@"[А-Я]\S*");//создание новой строки, где слово начинается только с заглавной буквы
+
+            //вывод элементов, удовлетворяющих условию
+            foreach (var i in str.Matches(a))
+            {
+
+                Console.WriteLine(i);
+
+            }    
+            
+
+        }
+    }
+}
